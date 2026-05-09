462a57d7ff865229aa8ab1162f1896a31b803356
CyberSqu1rr3l <stealthysquirrel403@mailfence.com>
2026-05-09 11:37:33 +0200

    Obtain password outside of the loop optimization

--- over_the_wire/bandit/level25/bruteforce_four_digits.sh
+++ over_the_wire/bandit/level25/bruteforce_four_digits.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
- 
+
+PASSWORD=$(cat /etc/bandit_pass/bandit24)
 {
 for i in {0000..9999}; do
-   PASSWORD=$(cat /etc/bandit_pass/bandit24)
-   echo "$PASSWORD $i"
+  echo "$PASSWORD $i"
 done
 } | nc localhost 30002 | grep -m2 "bandit25"
